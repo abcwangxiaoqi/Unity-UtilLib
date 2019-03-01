7d426cca79f09d0d4734ee8b627bd461734f2c0a
frankWang <489280591@qq.com>
2019-03-01 14:17:24 +0800

    1

--- Assets/UtilLib/ObjectPares/Editor/ParseModule/IObjectParse.cs
+++
@@ -1,8 +0,0 @@
-﻿using Object = UnityEngine.Object;
-
-public interface IObjectParse
-{
-    Object Create(string fold, string filename);
-    object Parse(Object source);
-    void Save(Object source,object obj);
-}
